326b4c7b82cecea2ed21ad6d77a63763eca6986d
harshithld0141-rgb <harshithld0141@gmail.com>
2026-05-25 11:08:44 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World! This is for webhooks");
+        System.out.println("Hello, 1! This is for webhooks");
     }
 }
